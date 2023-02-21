255a7ed10fda9f3d0487f9cf39d0c378040f1fc1
Satya <satran004@gmail.com>
2023-02-21 15:23:17 +0800

    Derive contract address using PlutusScriptUtil

--- off-chain/src/main/java/org/cardanofoundation/hydrapoc/batch/VoteUtxoFinder.java
+++ off-chain/src/main/java/org/cardanofoundation/hydrapoc/batch/VoteUtxoFinder.java
@@ -3,15 +3,18 @@ package org.cardanofoundation.hydrapoc.batch;
 import com.bloxbean.cardano.client.api.UtxoSupplier;
 import com.bloxbean.cardano.client.api.common.OrderEnum;
 import com.bloxbean.cardano.client.api.model.Utxo;
+import com.bloxbean.cardano.client.exception.CborSerializationException;
 import com.bloxbean.cardano.client.util.HexUtil;
 import com.bloxbean.cardano.client.util.Tuple;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.cardanofoundation.hydrapoc.commands.PlutusScriptUtil;
 import org.cardanofoundation.hydrapoc.importvote.VoteDatum;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,11 +24,16 @@ import java.util.stream.Collectors;
 @Slf4j
 public class VoteUtxoFinder {
     private final UtxoSupplier utxoSupplier;
-
-    @org.springframework.beans.factory.annotation.Value("${voting.batch.contract}")
-    private String voteBatchContractAddress;
+    private final PlutusScriptUtil plutusScriptUtil;
 
     public List<Tuple<Utxo, VoteDatum>> getUtxosWithVotes(int batchSize) {
+        String voteBatchContractAddress = null;
+        try {
+            voteBatchContractAddress = plutusScriptUtil.getVoteBatcherContractAddress();
+        } catch (CborSerializationException e) {
+            log.error("Error", e);
+            return Collections.EMPTY_LIST;
+        }
         boolean isContinue = true;
         List<Tuple<Utxo, VoteDatum>> utxos = new ArrayList<>();
         while (isContinue) {
