56fcae65d0ea74a9d70def1272285e057816b4d1
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-03-21 18:36:15 +0100

    next iteration

--- off-chain/src/test/java/org/cardanofoundation/hydrapoc/HashedListTest.java
+++ off-chain/src/test/java/org/cardanofoundation/hydrapoc/HashedListTest.java
@@ -1,66 +1,120 @@
 package org.cardanofoundation.hydrapoc;
 
+import com.bloxbean.cardano.client.plutus.api.PlutusObjectConverter;
+import com.bloxbean.cardano.client.plutus.impl.DefaultPlutusObjectConverter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.val;
+import org.cardanofoundation.hydrapoc.importvote.VoteDatum;
+import org.cardanofoundation.list.HashedList;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.bloxbean.cardano.client.util.HexUtil.encodeHexString;
+import static org.cardanofoundation.util.Hashing.sha2_256;
+
 public class HashedListTest {
 
-    public void test_001() {
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
+    private final static PlutusObjectConverter PLUTUS_OBJECT_CONVERTER = new DefaultPlutusObjectConverter();
 
+    private final String votesJson = """
+            [ {
+                    "voterKey" : "E9Y8mwRHjS/D4PlVP3kJSoMRHjrQPz93n1umpvN6xkc=",
+                    "votingPower" : 5540767,
+                    "challenge" : 258650178,
+                    "proposal" : 2888851,
+                    "choice" : 0
+                    }, {
+                    "voterKey" : "to6/Ff1uR99D+iwRwnNaTl41nPOvmvuTtfcy/5vvLB8=",
+                    "votingPower" : 2604515,
+                    "challenge" : 374145371,
+                    "proposal" : 2675120,
+                    "choice" : 0
+                    }, {
+                    "voterKey" : "Qbs5tHLTh1tud9ooQXguZe/7+FRHmCzRUpfrwoo0iAk=",
+                    "votingPower" : 253840,
+                    "challenge" : 72140352,
+                    "proposal" : 3057554,
+                    "choice" : 0
+                    }, {
+                    "voterKey" : "4ECxMuNvribHsNcnwuhZuFrla3FpsA4ATz0nzQ4/0/4=",
+                    "votingPower" : 4654383,
+                    "challenge" : 59549376,
+                    "proposal" : 3637300,
+                    "choice" : 1
+                    }, {
+                    "voterKey" : "GwJvm5sNFy38L6SkUFaf6M8QqDzithbrHc1feG6hmbA=",
+                    "votingPower" : 3353282,
+                    "challenge" : 214998680,
+                    "proposal" : 4269173,
+                    "choice" : 1
+                    }, {
+                    "voterKey" : "6I4lNR86I+vKHkyt92Cbagh/YdUlL4cK9d73V44btU4=",
+                    "votingPower" : 4620080,
+                    "challenge" : 116457209,
+                    "proposal" : 617559,
+                    "choice" : 2
+                    }, {
+                    "voterKey" : "E9Y8mwRHjS/D4PlVP3kJSoMRHjrQPz93n1umpvN6xkc=",
+                    "votingPower" : 5540767,
+                    "challenge" : 214998680,
+                    "proposal" : 1123389,
+                    "choice" : 0
+                    }, {
+                    "voterKey" : "bPbxSoMUXuRHwWXeW09Sn2a9VSKyaqjAOONS7QtqQbw=",
+                    "votingPower" : 881560,
+                    "challenge" : 258650178,
+                    "proposal" : 442754,
+                    "choice" : 2
+                    }, {
+                    "voterKey" : "to6/Ff1uR99D+iwRwnNaTl41nPOvmvuTtfcy/5vvLB8=",
+                    "votingPower" : 2604515,
+                    "challenge" : 72140352,
+                    "proposal" : 5776693,
+                    "choice" : 2
+                    }, {
+                    "voterKey" : "6I4lNR86I+vKHkyt92Cbagh/YdUlL4cK9d73V44btU4=",
+                    "votingPower" : 4620080,
+                    "challenge" : 59549376,
+                    "proposal" : 1247152,
+                    "choice" : 1
+                    } ]
+            """;
+
+    @Test
+    public void test_001() throws JsonProcessingException {
+        val temp_items = objectMapper.readValue(votesJson, new TypeReference<List<VoteDatum>>() {});
+
+        val items = List.of(
+                temp_items.get(0),
+                temp_items.get(1),
+                temp_items.get(2),
+                temp_items.get(9)
+//                temp_items.get(3),
+//                temp_items.get(4),
+//                temp_items.get(5)
+        );
+
+        //items.stream().map(voteDatum -> encodeHexString(voteDatum.getVoterKey())).forEach(System.out::println);
+
+//        items.forEach(voteDatum -> {
+//            System.out.println(String.format("""
+//                    Vote(
+//                            #"%s",
+//                            %d,
+//                            %d,
+//                            %d,
+//                            0,
+//                            )
+//                    """, encodeHexString(voteDatum.getVoterKey()), voteDatum.getVotingPower(), voteDatum.getChallenge(), voteDatum.getProposal(), voteDatum.getChoice()));
+//        });
+
+        val hashedList = HashedList.create(items, (v) -> sha2_256(PLUTUS_OBJECT_CONVERTER.toPlutusData(v).serializeToBytes()));
+        System.out.println(encodeHexString(hashedList.hash()));
     }
 
 }
-
-//        "results" : {
-//        "ChallengeProposalDatum(challenge=258650178, proposal=2888851)" : {
-//        "yes" : 0,
-//        "no" : 0,
-//        "abstain" : 5540767
-//        },
-//        "ChallengeProposalDatum(challenge=374145371, proposal=2675120)" : {
-//        "yes" : 0,
-//        "no" : 0,
-//        "abstain" : 2604515
-//        },
-//        "ChallengeProposalDatum(challenge=72140352, proposal=3057554)" : {
-//        "yes" : 0,
-//        "no" : 0,
-//        "abstain" : 253840
-//        },
-//        "ChallengeProposalDatum(challenge=59549376, proposal=3637300)" : {
-//        "yes" : 0,
-//        "no" : 4654383,
-//        "abstain" : 0
-//        },
-//        "ChallengeProposalDatum(challenge=214998680, proposal=4269173)" : {
-//        "yes" : 0,
-//        "no" : 3353282,
-//        "abstain" : 0
-//        },
-//        "ChallengeProposalDatum(challenge=116457209, proposal=617559)" : {
-//        "yes" : 4620080,
-//        "no" : 0,
-//        "abstain" : 0
-//        },
-//        "ChallengeProposalDatum(challenge=214998680, proposal=1123389)" : {
-//        "yes" : 0,
-//        "no" : 0,
-//        "abstain" : 5540767
-//        },
-//        "ChallengeProposalDatum(challenge=258650178, proposal=442754)" : {
-//        "yes" : 881560,
-//        "no" : 0,
-//        "abstain" : 0
-//        },
-//        "ChallengeProposalDatum(challenge=72140352, proposal=5776693)" : {
-//        "yes" : 2604515,
-//        "no" : 0,
-//        "abstain" : 0
-//        },
-//        "ChallengeProposalDatum(challenge=59549376, proposal=1247152)" : {
-//        "yes" : 0,
-//        "no" : 4620080,
-//        "abstain" : 0
-//        }
-//        },
-//        "merkleRootHash" : "rXX2FxuX5WVYSiqwsucKnz8hOQcgC1CSchF0kft1J+Y=",
-//        "iteration" : 0
-//        }
